39690cf4a72598520492c088fdd4a85cee409682
FelipeGdasilva <felipegomes35@yahoo.com.br>
2026-01-19 10:26:03 -0300

    atualização

---
+++ src/assets/index.js
@@ -0,0 +1,59 @@
+import sonicImg from "./images/sonic.png";
+import amyImg from "./images/amy.png";
+import tailsImg from "./images/tails.png";
+import knucklesImg from "./images/knuckles.png";
+import shadowImg from "./images/shadow.png";
+import supersonicImg from "./images/supersonic.png";
+import rougeImg from "./images/rouge.png";
+import creamImg from "./images/cream.png";
+import eggmanImg from "./images/eggman.png";
+
+import sonicIcon from "./icons/sonic-icon.png";
+import amyIcon from "./icons/amy-icon.png";
+import tailsIcon from "./icons/tails-icon.png";
+import shadowIcon from "./icons/shadow-icon.png";
+import supersonicIcon from "./icons/supersonic-icon.png";
+import knucklesIcon from "./icons/knuckles-icon.png";
+import rougeIcon from "./icons/rouge-icon.png";
+import creamIcon from "./icons/cream-icon.png";
+import eggmanIcon from "./icons/eggman-icon.png";
+
+import sonicSound from "./sounds/sonic.mp3";
+import amySound from "./sounds/amy.mp3";
+import shadowSound from "./sounds/shadow.mp3";
+import tailsSound from "./sounds/tails.mp3";
+import supersonicSound from "./sounds/supersonic.mp3";
+import knucklesSound from "./sounds/knuckles.mp3";
+import rougeSound from "./sounds/rouge.mp3";
+import creamSound from "./sounds/cream.mp3";
+import eggmanSound from "./sounds/eggman.mp3";
+
+export {
+    sonicImg,
+    tailsImg,
+    amyImg,
+    shadowImg,
+    supersonicImg,
+    knucklesImg,
+    rougeImg,
+    eggmanImg,
+    creamImg,
+    sonicIcon,
+    tailsIcon,
+    knucklesIcon,
+    shadowIcon,
+    amyIcon,
+    rougeIcon,
+    supersonicIcon,
+    eggmanIcon,
+    creamIcon,
+    sonicSound,
+    tailsSound,
+    knucklesSound,
+    amySound,
+    supersonicSound,
+    rougeSound,
+    shadowSound,
+    eggmanSound,
+    creamSound,
+}
--- src/data/characters.js
+++ src/data/characters.js
@@ -1,67 +1,96 @@
+import  { 
+    sonicImg,
+    tailsImg,
+    amyImg,
+    shadowImg,
+    supersonicImg,
+    knucklesImg,
+    rougeImg,
+    eggmanImg,
+    creamImg,
+    sonicIcon,
+    tailsIcon,
+    knucklesIcon,
+    shadowIcon,
+    amyIcon,
+    rougeIcon,
+    supersonicIcon,
+    eggmanIcon,
+    creamIcon,
+    sonicSound,
+    tailsSound,
+    knucklesSound,
+    amySound,
+    supersonicSound,
+    rougeSound,
+    shadowSound,
+    eggmanSound,
+    creamSound,
+}from "../assets";
 
 const characters = [
   {
     id: 1,
     name: "Sonic the Hedgehog",
-    icon: `${process.env.PUBLIC_URL}/icons/sonic-icon.png`,
-    image: `${process.env.PUBLIC_URL}/images/sonic.png`,
-    sound: `${process.env.PUBLIC_URL}/sounds/sonic.mp3`
+    icon:sonicIcon,
+    image:sonicImg, 
+    sound:sonicSound
   },
   {
     id: 2,
     name: "Miles Tails Prower",
-    icon: `${process.env.PUBLIC_URL}/icons/tails-icon.png`,
-    image: `${process.env.PUBLIC_URL}/images/Tails.png`,
-    sound: `${process.env.PUBLIC_URL}/sounds/tails.mp3`
+    icon: tailsIcon,
+    image:tailsImg,
+    sound: tailsSound
   },
   {
     id: 3,
     name: "Knuckles the Echidna",
-    icon: `${process.env.PUBLIC_URL}/icons/knuckles-icon.png`,
-    image: `${process.env.PUBLIC_URL}/images/knuckles.png`,
-    sound: `${process.env.PUBLIC_URL}/sounds/knuckles.mp3`
+    icon: knucklesIcon,
+    image: knucklesImg,
+    sound: knucklesSound
   },
   {
     id: 4,
     name: "Shadow the Hedgehog",
-    icon: `${process.env.PUBLIC_URL}/icons/shadow-icon.png`,
-    image: `${process.env.PUBLIC_URL}/images/shadow.png`,
-    sound: `${process.env.PUBLIC_URL}/sounds/shadow.mp3`
+    icon: shadowIcon,
+    image: shadowImg,
+    sound: shadowSound
   },
   {
     id: 5,
     name: "Supersonic",
-    icon: `${process.env.PUBLIC_URL}/icons/supersonic-icon.png`,
-    image: `${process.env.PUBLIC_URL}/images/supersonic.png`,
-    sound: `${process.env.PUBLIC_URL}/sounds/supersonic.Mp3`
+    icon: supersonicIcon,
+    image: supersonicImg,
+    sound: supersonicSound
   },
   {
     id: 6,
     name: "Rouge the Bat",
-    icon: `${process.env.PUBLIC_URL}/icons/rouge-icon.png`,
-    image: `${process.env.PUBLIC_URL}/images/rouge.png`,
-    sound: `${process.env.PUBLIC_URL}/sounds/rouge.Mp3`
+    icon: rougeIcon,
+    image: rougeImg,
+    sound: rougeSound
   },
   {
     id: 7,
     name: "Dr.Eggman",
-    icon: `${process.env.PUBLIC_URL}/icons/eggman-icon.png`,
-    image: `${process.env.PUBLIC_URL}/images/eggman.png`,
-    sound: `${process.env.PUBLIC_URL}/sounds/eggman.Mp3`
+    icon: eggmanIcon,
+    image: eggmanImg,
+    sound: eggmanSound
   },
   {
     id: 8,
     name: "amy Rose",
-    icon: `${process.env.PUBLIC_URL}/icons/amy-icon.png`,
-    image: `${process.env.PUBLIC_URL}/images/amy.png`,
-    sound: `${process.env.PUBLIC_URL}/sounds/amy.mp3`
+    icon:  amyIcon,
+    image: amyImg,
+    sound: amySound
   },
   {
     id: 9,
     name: "cream the Rabbit",
-    icon: `${process.env.PUBLIC_URL}/icons/cream-icon.png`,
-    image: `${process.env.PUBLIC_URL}/images/cream.png`,
-    sound: `${process.env.PUBLIC_URL}/sounds/cream.Mp3`
+    icon: creamIcon,
+    image:  creamImg,
+    sound: creamSound
   }
   
 ];
