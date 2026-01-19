bd36001cc6d1dcb175fe23e0682beeddfb892e1a
FelipeGdasilva <felipegomes35@yahoo.com.br>
2026-01-19 00:52:21 -0300

    adicionado o projeto

--- src/data/characters.js
+++ src/data/characters.js
@@ -12,56 +12,56 @@ const characters = [
     name: "Miles Tails Prower",
     icon: `${process.env.PUBLIC_URL}/icons/tails-icon.png`,
     image: `${process.env.PUBLIC_URL}/images/tails.png`,
-    sound: "/sounds/tails.mp3"
+    sound: `${process.env.PUBLIC_URL}/sounds/tails.mp3`
   },
   {
     id: 3,
     name: "Knuckles the Echidna",
     icon: `${process.env.PUBLIC_URL}/icons/knuckles-icon.png`,
     image: `${process.env.PUBLIC_URL}/images/knuckles.png`,
-    sound: "/sounds/kunckles.mp3"
+    sound: `${process.env.PUBLIC_URL}/sounds/knuckles.mp3`
   },
   {
     id: 4,
     name: "Shadow the Hedgehog",
     icon: `${process.env.PUBLIC_URL}/icons/shadow-icon.png`,
     image: `${process.env.PUBLIC_URL}/images/shadow.png`,
-    sound: "/sounds/shadow.mp3"
+    sound: `${process.env.PUBLIC_URL}/sounds/shadow.mp3`
   },
   {
     id: 5,
     name: "Supersonic",
-    icon: `${process.env.PUBLIC_URL} /icons/supersonic-icon.png`,
+    icon: `${process.env.PUBLIC_URL}/icons/supersonic-icon.png`,
     image: `${process.env.PUBLIC_URL}/images/supersonic.png`,
-    sound: "/sounds/supersonic.mp3"
+    sound: `${process.env.PUBLIC_URL}/sounds/supersonic.mp3`
   },
   {
     id: 6,
     name: "Rouge the Bat",
     icon: `${process.env.PUBLIC_URL}/icons/rouge-icon.png`,
     image: `${process.env.PUBLIC_URL}/images/rouge.png`,
-    sound: "/sounds/rouge.mp3"
+    sound: `${process.env.PUBLIC_URL}/sounds/rouge.mp3`
   },
   {
     id: 7,
     name: "Dr.Eggman",
     icon: `${process.env.PUBLIC_URL}/icons/eggman-icon.png`,
     image: `${process.env.PUBLIC_URL}/images/eggman.png`,
-    sound: "/sounds/eggman.mp3"
+    sound: `${process.env.PUBLIC_URL}/sounds/eggman.mp3`
   },
   {
     id: 8,
     name: "amy Rose",
     icon: `${process.env.PUBLIC_URL}/icons/amy-icon.png`,
     image: `${process.env.PUBLIC_URL}/images/amy.png`,
-    sound: "/sounds/amy.mp3"
+    sound: `${process.env.PUBLIC_URL}/sounds/amy.mp3`
   },
   {
     id: 9,
     name: "cream the Rabbit",
     icon: `${process.env.PUBLIC_URL}/icons/cream-icon.png`,
     image: `${process.env.PUBLIC_URL}/images/cream.png`,
-    sound: "/sounds/cream.mp3"
+    sound: `${process.env.PUBLIC_URL}/sounds/cream.mp3`
   }
   
 ];
